f71da02f20d880d96859f92cfda09ef10a5c1f0f
Tom Reed <t.reed1104@gmail.com>
2020-11-27 10:05:54 +0000

    Migrated tests

---
+++ api/tests/test_feature_list_rooms.py
@@ -0,0 +1,17 @@
+import json
+import unittest
+from shared import db
+from main import app
+
+class FeatureTest_RoomList(unittest.TestCase):
+    ## Setup Functions
+    def setUp(self):
+        app.config.from_pyfile('configs/testing.cfg')
+        self.app = app.test_client()
+        with app.app_context():
+            db.create_all()
+    ## Teardown
+    def tearDown(self):
+        with app.app_context():
+            db.session.remove()
+            db.drop_all()
