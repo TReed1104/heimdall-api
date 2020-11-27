c2167d1ab4601afabfde4d9a6dca6d911d6acead
Tom Reed <t.reed1104@gmail.com>
2020-11-27 10:06:27 +0000

    Migrated main.py

--- api/main.py
+++ api/main.py
@@ -2,9 +2,10 @@
 from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
-from models import example_model
+from models import computer, room, desk
 from shared import db
-from resources import template_resource
+from resources import list_computers, list_desks, list_rooms, list_availability
+from resources import handler_computers, handler_desks, handler_rooms
 
 ## Create our Flask app and connect it to our database
 app = Flask(__name__)
@@ -16,7 +17,13 @@ with app.app_context():
     db.create_all()
 
 ## Register the resources and the endpoints to access them
-api.add_resource(template_resource.TemplateResource, '/example')
+api.add_resource(list_computers.ComputerList, '/computers')
+api.add_resource(list_desks.DeskList, '/desks')
+api.add_resource(list_rooms.RoomList, '/rooms')
+api.add_resource(list_availability.AvailabilityList, '/availability')
+api.add_resource(handler_computers.ComputerHandler, '/computer_handler')
+api.add_resource(handler_desks.DeskHandler, '/desk_handler')
+api.add_resource(handler_rooms.RoomHandler, '/room_handler')
 
 if __name__ == '__main__':
     ## Initialise the application, 0.0.0.0 means to use our machine ip and enable debugging if needed
